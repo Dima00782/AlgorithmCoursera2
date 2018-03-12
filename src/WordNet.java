756f0cbfd5b1c20ec06d1bda395991d8ef841357
Dmitry Bezhetskov <dima00782@gmail.com>
2018-03-12 19:44:10 +0700

    implement constructor

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,6 +1,50 @@
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class WordNet {
+    private Map<String, Integer> synToId = new TreeMap<>();
+    private ArrayList<ArrayList<Integer>> links = new ArrayList<>();
+
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        if (synsets == null || hypernyms == null) {
+            throw new IllegalArgumentException();
+        }
+
+        In synsetIn = new In(synsets);
+        int kSynsets = 0;
+        while (synsetIn.hasNextLine()) {
+            String line = synsetIn.readLine();
+            String[] data = line.split(",");
+
+            assert data.length == 3;
+
+            int id = Integer.parseInt(data[0]);
+            String[] synset = data[1].split("\\s+");
+            for (String aSynset : synset) {
+                synToId.put(aSynset, id);
+            }
+
+            ++kSynsets;
+        }
+
+        for (int i = 0; i < kSynsets; ++i) {
+            links.add(new ArrayList<>());
+        }
+
+        In hypernymsIn = new In(hypernyms);
+        while (hypernymsIn.hasNextLine()) {
+            String line = hypernymsIn.readLine();
+            String[] data = line.split(",");
+
+            int id = Integer.parseInt(data[0]);
+            for (int i = 1; i < data.length; ++i) {
+                links.get(id).add(Integer.parseInt(data[i]));
+            }
+        }
     }
 
     // returns all WordNet nouns
@@ -26,5 +70,6 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
+        WordNet wn = new WordNet(args[0], args[1]);
     }
 }
